862e8281848f9b7eba7f6c1380c8d6c2ecc303d5
Alysson Oliveira <3oalysson@gmail.com>
2023-10-16 17:51:05 -0300

    Inicial

---
+++ src/main/java/com/senac/senacswagger/SenacSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.senac.senacswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenacSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senac/senacswagger/SenacSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.senacswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenacSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
